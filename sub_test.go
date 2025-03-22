56dd25fed418012f3cc9b3980433bd20d2baf69c
宋子桓🌈 <songzihuan@song-zh.com>
2025-03-22 18:20:37 +0800

    添加测试和构建流程
    新增了减法测试用例，并调整了主函数以支持多个参数的加法。同时，重构了GitHub Actions工作流，增加了针对不同操作系统的测试任务，并在合并PR时自动构建和发布新版本。

---
+++ sub_test.go
@@ -0,0 +1,31 @@
+// Copyright 2025 TestGithubAction Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import "testing"
+
+func TestSub(t *testing.T) {
+	test1 := t.Run("3-2", func(t *testing.T) {
+		if Sub(3, 2) != 1 {
+			t.Fatalf("Running test %s error!", t.Name())
+		}
+	})
+
+	test2 := t.Run("10-6", func(t *testing.T) {
+		if Sub(10, 6) != 4 {
+			t.Fatalf("Running test %s error!", t.Name())
+		}
+	})
+
+	if !test1 || !test2 {
+		t.Fail()
+	}
+
+	if t.Failed() {
+		t.Logf("Run test %s failed.", t.Name())
+	} else {
+		t.Logf("Run test %s success.", t.Name())
+	}
+}
